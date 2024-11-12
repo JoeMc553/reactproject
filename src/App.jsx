e1ea96bf126a68fddae6cf2164b5c321b723ce27
JoeMc553 <josephmccurtain553@gmail.com>
2024-11-12 16:42:45 -0600

    shadcntest

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import './App.css'
 import Navbar from "./components/Navbar";
 import studylogosmaller from './assets/studylogosmaller.png'
 import Signupbutton from "./components/Signupbutton";
-
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
       <>
+      <Button>asdfasd</Button>
     <a><img src={studylogosmaller}/></a>
       <Navbar/>
       <form>
